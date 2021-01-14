f6474185428d1a62dd0c3b1fce91a877d9b1aa56
autocode-test-user-2 <70654538+autocode-test-user-2@users.noreply.github.com>
2021-01-14 17:24:24 +0300

    Update abs.js

--- src/abs.js
+++ src/abs.js
@@ -1,4 +1,5 @@
 length = 3;
 length = 4;
-for (var i = 0; i < length; i++) {}  // Empty on purpose or missing piece of code ?
+for (var i = 0; i < length; i++) {}
+// comment
 module.exports = num => num < 0 ? num * -1 : num;
